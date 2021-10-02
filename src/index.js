381545ddc9f1d933753f6fd255f2527becf0281f
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-02 18:41:43 +0300

    setup express
    setup express appliccation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('works');
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.all('/', (req, res) => {
+    res.send('Its working yeyy!!')
+})
+
+app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server, press ctr + C.`) });
