411f7f0abe1b965212d735812b123e0e939ed523
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-02 19:09:49 +0300

    added views files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.all('/', (req, res) => {
-    res.send('Its working yeyy!!')
+    res.send('Its working yeyy!!');
 })
 
 app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server, press ctr + C.`) });
