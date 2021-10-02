e3bcd3a40b7d7f85cfc4b8a92e5c74e2f1b0cf9c
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-02 19:22:47 +0300

    add view express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 const port = 5000;
 
+app.set('views', path.resolve('./src/views'));
+
+app.engine('hbs', handlebars({
+    extend: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
 app.all('/', (req, res) => {
-    res.send('Its working yeyy!!');
+    res.render('index', { layout: false });
 })
 
 app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server, press ctr + C.`) });
