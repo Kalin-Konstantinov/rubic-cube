d8d9de0521d3199b087c88bc3581f79f920cb0dc
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-02 21:05:51 +0300

    fix some typos

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ app.all('/', (req, res) => {
     res.render('index');
 })
 
-app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server, press ctr + C.`) });
+app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server press ctrl + C.`) });
