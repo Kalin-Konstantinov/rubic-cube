bd9ed4f339a0a62933edcffdf124e127fcbfcbcd
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-03 07:10:28 +0300

    connect details page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.static('./src/static'));
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
 app.get('/create', (req, res) => {
     res.render('create');
@@ -18,6 +18,10 @@ app.get('/create', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about');
-})
+});
+
+app.get('/details/:id', (req, res) => {
+    res.render('details');
+});
 
 app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server press ctrl + C.`) });
