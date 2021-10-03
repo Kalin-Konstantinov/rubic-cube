13ae44bf8700387ee3703959b28817952827cc04
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-03 07:06:46 +0300

    connect create view

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const app = express();
 const port = 5000;
 
 handlebarsInitilization(app);
-app.use(express.static('./src/static'))
+app.use(express.static('./src/static'));
 
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.get('/create', (req, res) => {
+    res.render('create');
 })
 
 app.listen(port, () => { console.log(`Server is runing on port http://localhost:${port}... \nTo stop the server press ctrl + C.`) });
