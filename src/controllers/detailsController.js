5f3e3748c83beee9bd55713d89de428780bcf8c9
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-04 16:40:36 +0300

    fix details page
    details page load data from fake db now

--- src/controllers/detailsController.js
+++ src/controllers/detailsController.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const allCubes = require('../db/dbCubes');
 const router = express.Router();
 
 router.get('/:id', (req, res) => {
-    res.render('details', { title: 'Details' });
+    const id = req.params.id;
+    const cube = allCubes.filter(x => x.id == id);
+    res.render('details', { title: 'Details', ctx: cube[0] });
 });
-
 
 module.exports = router;
