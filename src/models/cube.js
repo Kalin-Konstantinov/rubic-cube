6893ec5f3f47d9c52fc080a41ff14bcbe2867a31
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-05 18:54:49 +0300

    add cube model

---
+++ src/models/cube.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 100,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => {
+                /^https?:\/\//i.test(value)
+            },
+            message: (props) => `${props.value} is not corret url address.`
+        }
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        max: 6,
+        min: 1,
+    },
+});
+
+const Cube = mongoose.model('Cube', cubeSchema);
+
+module.exports = Cube;
