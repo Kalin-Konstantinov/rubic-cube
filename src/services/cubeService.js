7ee5f708b378afb933d1c6ca9eb81c8a64fae472
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-05 16:49:39 +0300

    return stage so homepage to work

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,9 +1,18 @@
-
-let addCube;
-let findCubeById;
-let getAllCubes;   
+const allCubes = require("../db/dbCubes");
 
 
+const addCube = (cube) => {
+    cube.id = uniqid();
+    allCubes.push(cube);
+}
+
+const findCubeById = (id) => {
+    return allCubes.filter(x => x.id == id)[0];
+}
+
+const getAllCubes = () => {
+    return allCubes
+}
 
 module.exports = {
     addCube,
