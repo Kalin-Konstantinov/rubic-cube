a1b855c75d6681e05a462709640d1b34f4a23000
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-06 09:30:51 +0300

    add accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add', (req, res) => {
-    res.send('add accessory page')
+    res.render('createAccessory')
 })
 
 module.exports = router;
