cbc56f71c32bed25a2674df9399fc8a14d8be014
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-06 11:53:56 +0300

    add post method to accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 router.get('/add', (req, res) => {
     res.render('createAccessory')
-})
+});
+
+router.post('/add', (req, res) => {
+    let a = req.body
+    console.log(a);
+    res.end()
+});
 
 module.exports = router;
