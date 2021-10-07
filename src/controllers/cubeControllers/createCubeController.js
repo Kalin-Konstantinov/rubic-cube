e14a8781aed8659f281237f03c9ecfae288f34dd
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-07 08:28:25 +0300

    separate cube controllers in cubeControllers directory

--- src/controllers/cubeControllers/createCubeController.js
+++ src/controllers/cubeControllers/createCubeController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dbCubes = require('../services/cubeService');
+const dbCubes = require('../../services/cubeService');
 const router = express.Router();
 
 router.get('/', (req, res) => {
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const home = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const cubeController = require('./controllers/cubeControllers/cubeController');
 const about = require('./controllers/aboutController');
 const accessory = require('./controllers/accessoryController');
 const notFound = require('./controllers/404Controller');
