57826cdb70755db3367dd623416dc499706d3572
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-07 06:48:47 +0300

    refactor path url to create cube page

--- src/controllers/createCubeController.js
+++ src/controllers/createCubeController.js
@@ -2,11 +2,11 @@ const express = require('express');
 const dbCubes = require('../services/cubeService');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/create-page', (req, res) => {
     res.render('create', { title: 'Create Cube' });
 });
 
-router.post('/', (req, res) => {
+router.post('/create', (req, res) => {
     dbCubes.addCube(req.body)
         .then(cube => {
             res.redirect('/');
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const home = require('./controllers/homeController');
-const create = require('./controllers/createController');
+const create = require('./controllers/createCubeController');
 const about = require('./controllers/aboutController');
 const details = require('./controllers/detailsController');
 const accessory = require('./controllers/accessoryController');
 const notFound = require('./controllers/404Controller');
 
 router.use('/home', home);
-router.use('/create', create);
+router.use('/cube', create);
 router.use('/about', about);
 router.use('/details', details);
 router.use('/accessory', accessory);
