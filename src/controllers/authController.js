4496248ba67f02323df0508a7cea0a81d89afa7b
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-12 22:14:34 +0300

    add login page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 
+const login = (req, res) => {
+    res.render('auth/login', { title: 'Login' });
+}
+
+router.get('/login', login)
 module.exports = router;
