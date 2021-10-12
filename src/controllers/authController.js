519bad83dd29adb5edd41fb7dbddc887cf01337e
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-12 22:18:22 +0300

    add register page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,5 +4,11 @@ const login = (req, res) => {
     res.render('auth/login', { title: 'Login' });
 }
 
-router.get('/login', login)
+const register = (req, res) => {
+    res.render('auth/register', { title: 'Register' });
+}
+
+router.get('/login', login);
+router.get('/register', register);
+
 module.exports = router;
