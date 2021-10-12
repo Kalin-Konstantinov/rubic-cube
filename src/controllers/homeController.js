cc39ab7aeb91780d9b60aedd525deb094c3e214a
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-12 13:33:48 +0300

    refactor code
    move filtering action from controller to service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,24 +9,12 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/search', (req, res) => {
-    dbCubes.getAllCubes()
-        .then((cubes) => {
-            let name = req.query.search;
-            let from = req.query.from;
-            let to = req.query.to;
-            let filtredCubes = cubes;
-            if (name) {
-                filtredCubes = filtredCubes.filter(x => x.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
-            }
-            if (from) {
-                filtredCubes = filtredCubes.filter(x => Number(x.difficultyLevel) >= Number(from))
-            }
-            if (to) {
-                filtredCubes = filtredCubes.filter(x => Number(x.difficultyLevel) <= Number(to))
-            }
-            res.render('index', { title: 'Home', ctx: filtredCubes });
-        })
+router.get('/search', async (req, res) => {
+    let name = req.query.search;
+    let from = req.query.from;
+    let to = req.query.to;
+    let filtredCubes = dbCubes.filterCubes(name, from, to);
+    res.render('index', { title: 'Home', ctx: filtredCubes });
 })
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -10,8 +10,24 @@ const findCubeById = (id) => Cube.findById(id).lean();
 
 const getAllCubes = () => Cube.find().lean();
 
+const filterCubes = async (name, from, to) => {
+
+    let filtredCubes = await Cube.find().lean();
+    if (name) {
+        filtredCubes = filtredCubes.filter(x => x.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
+    }
+    if (from) {
+        filtredCubes = filtredCubes.filter(x => Number(x.difficultyLevel) >= Number(from))
+    }
+    if (to) {
+        filtredCubes = filtredCubes.filter(x => Number(x.difficultyLevel) <= Number(to))
+    }
+    return filtredCubes;
+}
+
 module.exports = {
     addCube,
     findCubeById,
     getAllCubes,
+    filterCubes,
 };
