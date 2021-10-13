3b6784acbaa0b6b89ad4a6ccd4bb321c973a8ce6
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-13 19:22:01 +0300

    add bcrypt
    connect db with register form

--- src/models/user.js
+++ src/models/user.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         minLength: 5,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
+        minLength: [6, 'Your password should be at least 6 symbols long.'],
     }
 });
 
+userScerma.pre('save', async function (next) {
+    
+})
 
 const User = mongoose.model('User', userSchema);
 
