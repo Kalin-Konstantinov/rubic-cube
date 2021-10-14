8161a32d9bfbfa39ff884f925c988bf757c9464c
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-14 18:35:30 +0300

    fic create user function to async/await

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,8 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-const createUser = (username, password) => {
-    User.create({ username, password });
+const createUser = async (username, password) => {
+    await User.create({ username, password });
 }
 
 const login = (username, password) => {
