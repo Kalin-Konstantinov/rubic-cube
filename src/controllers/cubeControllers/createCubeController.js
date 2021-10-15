eada2623838b50bb60b7bb09acaf5d6afec24945
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-10-15 18:08:38 +0300

    add isAuth on create page

--- src/controllers/cubeControllers/createCubeController.js
+++ src/controllers/cubeControllers/createCubeController.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dbCubes = require('../../services/cubeService');
 const router = express.Router();
+const { isAuth } = require('../../middlewares/authMiddlewares');
+
+router.use(isAuth);
 
 const renderCreatePage = (req, res) => {
     res.render('create', { title: 'Create Cube' });
